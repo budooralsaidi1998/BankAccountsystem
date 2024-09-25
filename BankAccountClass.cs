26f5dc660675e1147ea8efd6cb2b5aa3c3a249b0
budoor <budooralsaidi1998@gmail.com>
2024-09-25 16:33:09 +0400

    add deposit , withdrow , getaccount function and add the getter and setter balance

--- BankAccountClass.cs
+++ BankAccountClass.cs
@@ -1,5 +1,7 @@
 ﻿
 
+using System.ComponentModel.Design;
+
 namespace Banksystem
 {
     public class BankAccountClass
@@ -7,7 +9,9 @@ namespace Banksystem
 
         private string AccountNumber;
         private string AccountHolderName;
-        private decimal Balance;
+        public decimal Balance { get; private set; }
+        public static decimal balanceamount = 0;
+
 
         public  BankAccountClass(string accountNumber ,string accountholdername )
         {
@@ -20,13 +24,41 @@ namespace Banksystem
             Balance = initialdeposit;
             AccountNumber = accountNumber;
             AccountHolderName = accountholdername;
-        }
+          
+            
 
+
+        }
+        //increase blanace
         public void deposit()
         {
+           
+            
+                balanceamount = +Balance;
+                Console.WriteLine($"the balance after deposir : {balanceamount}");
+            
 
         }
 
+        public void withdrow()
+        {
+            if (Balance > 5) 
+            { 
+            balanceamount = -Balance;
+            Console.WriteLine($"the balance after withdrow : {balanceamount}");
+            }
+            else
+            {
+                Console.WriteLine(" is insufficient balance");
+            }
+        
+        }
+        public void GetAccountInfo(decimal initialdeposit, string accountNumber, string accountholdername)
+        {
+            Console.WriteLine($"the account number : {accountNumber}");
+            Console.WriteLine($"the account holder name : {accountholdername}");
+            Console.WriteLine($"the balance : {initialdeposit}");
+        }
 
 
 
