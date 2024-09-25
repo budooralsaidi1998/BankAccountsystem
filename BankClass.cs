05cc2991ae7a0f1d18330559c9fac42c1e4c69d0
budoor <budooralsaidi1998@gmail.com>
2024-09-25 17:05:56 +0400

    add list

---
+++ BankClass.cs
@@ -0,0 +1,21 @@
+﻿
+
+using System.Collections.Specialized;
+
+namespace Banksystem
+{
+    public class BankClass
+    {
+
+    List<BankAccountClass>BankAccount =new List<BankAccountClass>();
+
+
+
+
+
+
+
+
+
+    }
+}
