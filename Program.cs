80caff8447300bf0a20270ea54b5b47c3a369eb3
budoor <budooralsaidi1998@gmail.com>
2024-09-25 15:48:36 +0400

    create class bankaccount

--- Program.cs
+++ Program.cs
@@ -4,7 +4,12 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("welcom in Bank account !!");
+
+            
+            BankAccountClass bank1 = new BankAccountClass("A12333","budoor");
+            BankAccountClass bank2 = new BankAccountClass(100,"A0001","Ali");
+
         }
     }
 }
