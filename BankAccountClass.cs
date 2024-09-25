ad1292b3d949b6ec6249c66ab2c35576d89fe6bb
budoor <budooralsaidi1998@gmail.com>
2024-09-25 15:49:23 +0400

    add attribut for class with construct

---
+++ BankAccountClass.cs
@@ -0,0 +1,34 @@
+﻿
+
+namespace Banksystem
+{
+    public class BankAccountClass
+    {
+
+        private string AccountNumber;
+        private string AccountHolderName;
+        private decimal Balance;
+
+        public  BankAccountClass(string accountNumber ,string accountholdername )
+        {
+            AccountNumber=accountNumber;
+            AccountHolderName=accountholdername;
+        }
+
+        public BankAccountClass(decimal initialdeposit, string accountNumber, string accountholdername)
+        {
+            Balance = initialdeposit;
+            AccountNumber = accountNumber;
+            AccountHolderName = accountholdername;
+        }
+
+        public void deposit()
+        {
+
+        }
+
+
+
+
+    }
+}
